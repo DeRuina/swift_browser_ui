1902897c8bff2fd35c0301652d078c92417e2971
Dean Ruina <dean.ruina@csc.fi>
2025-06-16 14:33:12 +0300

    statsd exporter middleware

--- deploy/app.sh
+++ deploy/app.sh
@@ -5,4 +5,4 @@ THE_PORT=${PORT:="8080"}
 
 echo 'Start object-browser application'
 
-gunicorn swift_browser_ui.ui.server:servinit --bind $THE_HOST:$THE_PORT --worker-class aiohttp.GunicornUVLoopWebWorker --workers 1 --graceful-timeout 60 --timeout 120 --statsd-host=grafana-statsd-exporter.allas-ui-dev.svc.cluster.local:9125 --statsd-prefix=swiftbrowser
+gunicorn swift_browser_ui.ui.server:servinit --bind $THE_HOST:$THE_PORT --worker-class aiohttp.GunicornUVLoopWebWorker --workers 1 --graceful-timeout 60 --timeout 120
