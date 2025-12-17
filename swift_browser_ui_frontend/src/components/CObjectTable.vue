102c51575e6a06150db46b860ce3cbd3ba9be469
Dean Ruina <dean.ruina@csc.fi>
2025-12-17 17:03:34 +0200

    fix: parentheses access denied

--- swift_browser_ui_frontend/src/components/CObjectTable.vue
+++ swift_browser_ui_frontend/src/components/CObjectTable.vue
@@ -271,6 +271,8 @@ export default {
                     const hasSpaceOrTab = /[ \t]/.test(item.name);
                     const hasNonAsciiBucket = /[^\x20-\x7E]/.test(this.container);
                     const hasSpaceOrTabBucket = /[ \t]/.test(this.container);
+                    const needsEncodingName = encodeURIComponent(item.name) !== item.name;
+                    const needsEncodingBucket = encodeURIComponent(this.container) !== this.container;
 
                     const needsProxy =
                       isSharedRoute ||
@@ -279,7 +281,9 @@ export default {
                       hasNonAscii ||
                       hasSpaceOrTab ||
                       hasNonAsciiBucket ||
-                      hasSpaceOrTabBucket;
+                      hasSpaceOrTabBucket ||
+                      needsEncodingName ||
+                      needsEncodingBucket;
 
                     if (needsProxy) {
                       this.beginDownload(item, event.isTrusted); // proxy path
